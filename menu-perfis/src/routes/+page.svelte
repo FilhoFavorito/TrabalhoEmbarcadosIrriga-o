96408bd5417cd19cb1f8409679c6528cbac7948a
joao <joaopedroguesser902@gmail.com>
2024-12-13 18:45:56 -0300

    rota para pagina de adicionar perfil criada

--- menu-perfis/src/routes/+page.svelte
+++ menu-perfis/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+  .menu { font-family: Arial, sans-serif; text-align: center; margin: 50px;}
+  h1 { color: #333; }
+  p { font-size: 18px; }
+  .led { margin: 20px; }
+</style>
+
+<div class="menu">
+
+</div>
+  <h1>Monitor de Ambiente</h1>
+  <p><strong>Umidade:</strong>  varivavel aqui </p>
+  <p><strong>Temperatura:</strong> varivavel aqui </p>
+
+  <div class="led">
+    <a href="/addPerfil"><button>Adicionar Perfil</button></a>
+    <a href="/removePerfil"><button>Excluir Perfil</button></a>
+  </div>
